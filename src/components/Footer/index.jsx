d9508c433115f69d8383bc49564dc16c3bef8126
Breno Oliveira <breno.oliveira2011@hotmail.com>
2024-12-31 11:14:07 -0300

    criando as opções de escolha de dia, horario e forma de pagamento

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return ( 
-        <footer>
+        <footer className="mt-4">
             <div className="bg-primary">
           <div className="container mx-auto flex justify-center items-center p-4 px-8">
             <div className="text-background font-bold  lg:text-xl md:text-lg sm:text-md text-sm">2024 Dri Estética. Todos os direitos reservados.</div>
--- src/pages/Agendamento/index.jsx
+++ src/pages/Agendamento/index.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Section from "../../components/Section";
 import ServiceCard from "../../components/ServiceCard";
 import { services } from "../../database/services";
+import { SiPix } from "react-icons/si";
+import { FaCcMastercard, FaCreditCard, FaMoneyBillAlt } from "react-icons/fa";
 
 const Agendamento = () => {
   // Estado para armazenar o índice do card selecionado
@@ -42,46 +44,81 @@ const Agendamento = () => {
       </Section>
       {/* Exibe os serviços selecionados */}
       {servicesSelecteds.length > 0 && (
-        <div className="bg-background container px-10 py-2 mx-auto rounded-md shadow-md min-h-44 h-full">
-          <h2 className="text-2xl font-bold text-gray-800 mt-6">
-            Serviços Selecionados:
-          </h2>
-          <ul className="list-inside list-disc">
-            {servicesSelecteds.map((index) => (
-              <li key={index} className="text-md text-gray-600">
-                {services[index].title} - {services[index].price}
-              </li>
-            ))}
-          </ul>
+        <div className="bg-background container px-10 py-6 mx-auto rounded-md shadow-lg min-h-44 h-full grid grid-cols-1 md:grid-cols-4 gap-6">
+          {/* Coluna 1: Lista de serviços selecionados */}
+          <div className="col-span-4">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Serviços Selecionados:
+            </h2>
+            <ul className="list-inside list-disc">
+              {servicesSelecteds.map((index) => (
+                <li key={index} className="text-md text-gray-600">
+                  {services[index].title} - {services[index].price}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-          <div className="flex justify-between">
-            {/* Escolher Data */}
-            <div>
-              <h2 className="text-2xl font-bold text-gray-800 mt-6">
-                Escolha a Data e Horário
-              </h2>
-              <label className="text-md flex flex-col text-neutral font-semibold" htmlFor="date">
-                Data
-              <input className="w-1/3 border rounded-md p-2 text-black" id="date" type="date" />
-              </label>
+          {/* Coluna 2: Escolher Data */}
+          <div className="col-span-4 md:col-span-2">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Escolha a Data e Horário
+            </h2>
+            <label
+              className="text-md flex flex-col text-neutral font-semibold"
+              htmlFor="date"
+            >
+              Data
+              <input
+                className="border rounded-md p-2 text-black mt-2"
+                id="date"
+                type="date"
+              />
+            </label>
+          </div>
+
+          {/* Coluna 3: Horários Disponíveis */}
+          <div className="col-span-2">
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Horários Disponíveis
+            </h2>
+            <div className="grid grid-cols-2 gap-2">
+              <button className="rounded-md bg-gray-100 hover:bg-lightBackground text-md px-7 p-2">
+                09:00
+              </button>
+              <button className="rounded-md bg-gray-100 hover:bg-lightBackground text-md px-7 p-2">
+                10:00
+              </button>
+              <button className="rounded-md bg-gray-100 hover:bg-lightBackground text-md px-7 p-2">
+                11:00
+              </button>
+              <button className="rounded-md bg-gray-100 hover:bg-lightBackground text-md px-7 p-2">
+                12:00
+              </button>
             </div>
+          </div>
 
-            {/* Horários Disponiveis */}
-            <div className="flex flex-col px-20">
-              <h1>Horários Disponíveis</h1>
-              <div className="flex flex-wrap gap-2 ">
-                <button className="rounded-md bg-gray-100 hover:bg-gray-200 text-md px-7 p-1">
-                  09:99
-                </button>
-                <button className="rounded-md bg-gray-100 hover:bg-gray-200 text-md px-7 p-1">
-                  09:99
-                </button>
-                <button className="rounded-md bg-gray-100 hover:bg-gray-200 text-md px-7 p-1">
-                  09:99
-                </button>
-                <button className="rounded-md bg-gray-100 hover:bg-gray-200 text-md px-7 p-1">
-                  09:99
-                </button>
+          {/* Coluna 4: Método de Pagamento */}
+          <div className="col-span-4">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+              Escolha o Método de Pagamento
+            </h2>
+            <div className="grid grid-cols-4 gap-2">
+              <div className="w-full h-full gap-2 font-semibold p-6 border hover:border-secondary rounded-md flex flex-col items-center justify-center">
+              <SiPix size={22} className="text-cyan-500" />
+                <p>PIX</p>
+              </div>
+              <div className="w-full h-full gap-2 font-semibold p-6 border hover:border-secondary rounded-md flex flex-col items-center justify-center">
+              <FaCreditCard size={22} className="text-blue-500" />
+                <p>Débito</p>
+              </div>
+              <div className="w-full h-full gap-2 font-semibold p-6 border hover:border-secondary rounded-md flex flex-col items-center justify-center">
+              <FaCreditCard size={22} className="text-yellow-500" />
+                <p>Crédito</p>
+              </div>
+              <div className="w-full h-full gap-2 font-semibold p-6 border hover:border-secondary rounded-md flex flex-col items-center justify-center">
+              <FaMoneyBillAlt size={24} className="text-green-500" />
+                <p>Dinheiro</p>
               </div>
             </div>
           </div>
