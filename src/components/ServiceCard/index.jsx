d68f60e41a5a854fb05350b389f9f81d957e9ea4
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2025-01-16 10:50:45 -0300

    ajusta do tamanho do service card para telas menores

--- src/components/ServiceCard/index.jsx
+++ src/components/ServiceCard/index.jsx
@@ -1,7 +1,7 @@
 // ServiceCard.js
 const ServiceCard = ({ title, description, price, border, backgroundColor, onClick }) => {
   return (
-    <div onClick={onClick} className={`${backgroundColor} ${border} p-6 rounded-lg shadow-md sm:w-80 md:w-[450px] w-full h-full flex flex-col justify-between text-center`}>
+    <div onClick={onClick} className={`${backgroundColor} ${border} p-6 rounded-lg shadow-md sm:w-80 md:w-[450px] w-9/12 h-full flex flex-col justify-between text-center`}>
       <div>
         <h2 className="text-xl md:text-3xl font-bold text-primary mb-2">{title}</h2>
         <p className="text-neutral text-md md:text-lg lg:text-2xl mb-4">{description}</p>
